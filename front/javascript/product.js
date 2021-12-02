7432a9b40cb942fcdd069ab5dd1830760707aff2
Ninon85 <88130318+Ninon85@users.noreply.github.com>
2021-12-02 15:49:56 +0100

    début étape 7

--- front/javascript/product.js
+++ front/javascript/product.js
@@ -21,5 +21,72 @@ let headTitle = document.querySelector("title");
 //colors storage
 let colorsProduct;
 
+//tempory storage of client choice
+let quantityProduct;
+let colorChoice = "";
 getId();
 integrateDataHtml();
+
+let recapChoice = {
+	idProduct: id,
+	color: colorChoice,
+	quantityNumber: quantityProduct,
+};
+let localStorageArray = JSON.parse(localStorage.getItem("product"));
+//get value for quantity
+quantity.addEventListener("input", (e) => {
+	quantityProduct = parseInt(e.target.value);
+	recapChoice.quantityNumber = quantityProduct;
+});
+//get value of color
+selectColors.addEventListener("change", (e) => {
+	colorChoice = e.target.value;
+	recapChoice.color = colorChoice;
+	// keyLocalStorage.push(colorChoice);
+});
+
+//FUNCTION TO ADD CHOICE TO THE LOCAL STORAGE
+const addToLocaleStorage = () => {
+	localStorageArray.push(recapChoice);
+	localStorage.setItem("product", JSON.stringify(localStorageArray));
+};
+
+addToCart.addEventListener("click", () => {
+	//if no color selected
+	if (colorChoice === "") {
+		alert("Veuillez choisir une couleur");
+		//if no quantity
+	} else if (quantityProduct == 0) {
+		alert("Veuillez choisir une quantité comprise entres 0 et 100");
+		//if more 100
+	} else if (quantityProduct > 100) {
+		alert("Il n'est pas possible de commander plus de 100 articles");
+	}
+	//if localStorage is empty I create an array to put inside the client choices
+	else if (localStorageArray == null) {
+		localStorageArray = [];
+		addToLocaleStorage();
+		//If the array is not empty, I check wich index has ever the same id and the same color product
+	} else if (localStorageArray) {
+		let findIndex = localStorageArray.findIndex(
+			(i) =>
+				recapChoice.color === i.color && recapChoice.idProduct === i.idProduct
+		);
+		// console.log(findIndex);
+		//If a product has the same id and the same color, I make the sum of the quantity
+		if (findIndex !== -1) {
+			localStorageArray[findIndex].quantityNumber =
+				localStorageArray[findIndex].quantityNumber + quantityProduct;
+			//then I push the item in localStorage
+			localStorageArray.push(recapChoice);
+			localStorageArray.pop();
+			localStorage.setItem("product", JSON.stringify(localStorageArray));
+		} else {
+			addToLocaleStorage();
+		}
+	}
+
+	console.log(localStorageArray);
+});
+
+// localStorage.clear();
